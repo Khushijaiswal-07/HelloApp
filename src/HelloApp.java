c4b2db74fc5c6baf02b6ebdff458cef22d772b04
Khushijaiswal-07 <khushibtn@gmail.com>
2026-03-17 10:30:56 +0530

    Completed UC3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,12 +2,16 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
+        // Check if command-line argument exists
+        String name;
+
         if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
+            name = args[0];   // use the provided name
         } else {
-            System.out.println("Hello, World!");
+            name = "World";   // default value
         }
 
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
     }
-
 }
